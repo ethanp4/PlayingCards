acb2ce8c80c9ed70d7aa3e08e4e6260ee95fa2f5
Ethan <157554972+ethanp4@users.noreply.github.com>
2024-12-02 17:23:17 -0700

    Add project files.

---
+++ PlayingCards/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayingCards
+{
+    internal class Player
+    {
+        public string name {  get; set; }
+        public string team { get; set; }
+        public int gamesPlayed { get; set; }
+        public int points { get; set; }
+        public int goals { get; set; }
+        public int assists { get; set; }
+        public static Player[] players = { new Player { name = "Sam Reinhart", team = "FLA", gamesPlayed = 25, goals = 18, assists = 16, points = 32 },
+            new Player { name = "Leon Draisaitl", }
+        };
+    }
+}
